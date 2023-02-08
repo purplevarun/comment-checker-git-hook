4979c74d67c69c82403fc99647479ce97ce8717a
purplevarun <purplevarun@gmail.com>
2023-02-08 13:06:24 +0530

    fix installation and readme

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
-curl https://raw.githubusercontent.com/purplevarun/comment-checker-git-hook/main/hook.cc > hook-creator.cc
 g++ -std=c++11 hook-creator.cc -o pre-commit
 rm -rf .git/hooks/pre-commit
 mv pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
-rm -rf hook-creator.cc
