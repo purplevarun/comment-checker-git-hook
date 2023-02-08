b583d2134c9b5caeea43551750c1d2c729d9236c
purplevarun <purplevarun@gmail.com>
2023-02-08 12:51:06 +0530

    added executable in install script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 curl https://github.com/purplevarun/comment-checker-git-hook/blob/main/result/comment-checker-hook?raw=true > pre-commit
 rm -rf .git/hooks/pre-commit
 mv pre-commit .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
