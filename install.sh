94e6ef70a447fabeec8636de3dcdacee3c9186ba
purplevarun <purplevarun@gmail.com>
2023-02-09 12:42:59 +0530

    simplify install script

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
+# download the hook-creator
+curl https://raw.githubusercontent.com/purplevarun/comment-checker-git-hook/main/hook.cc > hook-creator.cc
+echo "downloaded hook-creator"
+
+# compile hook-creator and add hook
 g++ -std=c++11 hook-creator.cc -o pre-commit
 rm -rf .git/hooks/pre-commit
 mv pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
+echo "added hook in repository"
+
+# cleanup
+rm -rf hook-creator.cc
--- readme.md
+++ readme.md
@@ -17,9 +17,7 @@
 
 You can copy paste the below snippet in the home directory of your repository!
 
-    curl https://raw.githubusercontent.com/purplevarun/comment-checker-git-hook/main/hook.cc > hook-creator.cc
     curl https://raw.githubusercontent.com/purplevarun/comment-checker-git-hook/main/install.sh | bash
-    rm -rf hook-creator.cc
 
 ---
 
